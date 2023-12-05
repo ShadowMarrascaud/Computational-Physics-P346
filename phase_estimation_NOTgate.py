79141f5a36d06eb9dc0411669a038c549e67710a
ShadowMarrascaud <152955203+ShadowMarrascaud@users.noreply.github.com>
2023-12-05 13:16:38 +0530

    Update phase_estimation_NOTgate.py

--- phase_estimation_NOTgate.py
+++ phase_estimation_NOTgate.py
@@ -1,3 +1,16 @@
+#initialization
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+# importing Qiskit
+from qiskit import IBMQ, Aer, transpile, assemble
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+
+# import basic plot tools and circuits
+from qiskit.visualization import plot_histogram
+from qiskit.circuit.library import QFT
+
 #now for NOT gate
 
 qpe = QuantumCircuit(4, 3)
